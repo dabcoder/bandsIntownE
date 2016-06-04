012217fe6ca52c8f7829c772efa3e45491fb2a81
dbouchare <dbouchare@gmail.com>
2016-06-04 12:27:49 +0200

    Add /events/ route with code duplication

--- main.go
+++ main.go
@@ -1,3 +1,4 @@
+
 package main
 
 import (
@@ -21,8 +22,23 @@ func main() {
 		json.NewEncoder(w).Encode(data)
 	})
 
+
+	http.HandleFunc("/events/", func(w http.ResponseWriter, r *http.Request) {
+		artist := strings.SplitN(r.URL.Path, "/", 3)[2]
+
+		data, err := queryEvents(artist)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+        	return
+		}
+
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+		json.NewEncoder(w).Encode(data)
+	})
+
 	http.ListenAndServe(":8080", nil)
 }
+
 
 func query(artist string) (bandsData, error) {
 	resp, err := http.Get("http://api.bandsintown.com/artists/" + artist + ".json?api_version=2.0&app_id=bitgoapp")
@@ -42,7 +58,29 @@ func query(artist string) (bandsData, error) {
     return b, nil
 }
 
+func queryEvents(artist string) ([]eventsData, error) {
+	resp, err := http.Get("http://api.bandsintown.com/artists/" + artist + "/events.json?api_version=2.0&app_id=bitgoapp")
+	if err != nil {
+		return []eventsData{}, err
+	}
+
+	defer resp.Body.Close()
+
+	var e []eventsData
+
+	if err := json.NewDecoder(resp.Body).Decode(&e); err != nil {
+        return []eventsData{}, err
+    }
+
+    return e, nil
+}
+
 type bandsData struct {
 	Name string `json:"name"`
 	NumberEvents int `json:"upcoming_event_count"` 
+}
+
+type eventsData struct {
+	Location string `json:"formatted_location"`
+	Date string `json:"formatted_datetime"`
 }
